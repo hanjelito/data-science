2925a246b427459bbd9766a67dd62d70329cd899
Juan angel Gonzales laura <juan-gon@c3r13s6.42madrid.com>
2024-04-10 20:39:16 +0200

    2 graph

--- projects/module02/ex01/chart.py
+++ projects/module02/ex01/chart.py
@@ -4,6 +4,9 @@ import time
 from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+import matplotlib.dates as mdates
+
 
 def connect():
     db_url = 'postgresql+psycopg2://juan-gon:mysecretpassword@localhost:5432/piscineds'
@@ -16,68 +19,80 @@ except SQLAlchemyError as e:
 	print(f"Error: {e}")
     
 
-def columnes_count(table_name: str, start_date: str, end_date: str, event_type_filter: str) -> any:
+def create_query_date(table_name: str, new_table: str, start_date: str, end_date: str, event_type_filter: str) -> None:
     try:
         start_time = time.time()
-        assert engine.connect(), "Error: Could not connect to the database"
         with engine.connect() as conn:
             sql_command = f"""
-            SELECT DATE(event_time) AS event_day, COUNT(*) AS event_count
-			FROM {table_name}
-			WHERE event_time >= :start_date AND event_time < :end_date
-			AND event_type = :event_type_filter
-			GROUP BY event_day;
+            DROP TABLE IF EXISTS {new_table};
+            CREATE TABLE {new_table} AS
+            SELECT DATE(event_time) AS event_day,
+                   SUM(price) AS total_price,
+                   COUNT(*) AS event_count,
+                   COUNT(DISTINCT user_id) AS unique_users
+            FROM {table_name}
+            WHERE event_time >= :start_date AND event_time < :end_date
+            AND event_type = :event_type_filter
+            GROUP BY event_day;
             """
             result = conn.execute(text(sql_command), {'start_date': start_date, 'end_date': end_date, 'event_type_filter': event_type_filter})
-
+            conn.commit()
             print(f"Generated {result.rowcount} rows")
+        
         end_time = time.time()
         print(f"Elapsed time: {end_time - start_time:.2f} seconds")
-        return result.fetchall()
-
-    except AssertionError as e:
+    
+    except Exception as e:
         print(f"Error: {e}")
 
-def generate_time_series_chart(data):
-    df = pd.DataFrame(data, columns=['event_time', 'event_count'])
-    df['event_time'] = pd.to_datetime(df['event_time'])
-    df.set_index('event_time', inplace=True)
+def query_total_sales(table_name:str)-> any:
+    try:
+        with engine.connect() as conn:
+            sql_command = text(f"""
+            SELECT * 
+            FROM {table_name}
+            """)
+            result = conn.execute(sql_command)
+            return result.fetchall()
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
 
-    df.resample('D').sum().plot()
+def generate_time_series_chart(data):
+    df = pd.DataFrame(data, columns=['event_day', 'event_count', 'total_price', 'unique_users'])
+    df = df[['event_day', 'unique_users']]
+    df['event_day'] = pd.to_datetime(df['event_day'])
+    df.set_index('event_day', inplace=True)
+
+    df['unique_users'].resample('D').sum().plot(kind='line', color='blue')
+
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b'))
+    plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
+
     plt.ylabel('Number of costumers')
-    plt.xlabel('month')
-    plt.legend().remove()
+    plt.xlabel('')
     plt.tight_layout()
     plt.savefig('number_of_costumers.png')
     plt.close()
     
 def generate_monthly_sales_chart(data):
-    # Asegúrate de que los datos están en el formato correcto:
-    # [(date1, count1), (date2, count2), ...]
+    df = pd.DataFrame(data, columns=['event_day', 'event_count', 'total_price', 'unique_users'])
     
-    df = pd.DataFrame(data, columns=['event_day', 'event_count'])
-    
-    # Convertimos 'event_day' a datetime si no lo es ya
+    df.drop(columns=['unique_users'], inplace=True)
+
     df['event_day'] = pd.to_datetime(df['event_day'])
-    
-    # Truncamos las fechas al primer día de cada mes y agrupamos por esta nueva columna
     df['month'] = df['event_day'].dt.to_period('M')
     monthly_data = df.groupby('month')['event_count'].sum().reset_index()
-    
-    # Convertimos 'month' a cadena para que sea más legible
-    monthly_data['month'] = monthly_data['month'].dt.strftime('%b %Y')
-    
-    # Creamos el gráfico
+    monthly_data['month'] = monthly_data['month'].dt.strftime('%b')
+
+    monthly_data['event_count'] = monthly_data['event_count'] / 1e6
+
     ax = monthly_data.plot(kind='bar', x='month', y='event_count', legend=False, color='skyblue')
-    ax.set_title('Total Sales per Month')
     ax.set_xlabel('Month')
-    ax.set_ylabel('Total Sales')
-    
-    # Formateamos los ticks del eje y para que sean legibles
-    ax.get_yaxis().set_major_formatter(plt.FuncFormatter(lambda x, loc: "{:,}".format(int(x))))
-    
-    # Asegúrate de que las etiquetas del eje x sean legibles
-    ax.set_xticklabels(monthly_data['month'], rotation=45)
+    ax.set_ylabel('Total Sales Millions of ₳')
+
+    ax.get_yaxis().set_major_formatter(FuncFormatter(lambda x, loc: "{:,.1f}".format(x)))
+    ax.set_xticklabels(monthly_data['month'], rotation=0)
     
     plt.tight_layout()
     plt.savefig('monthly_sales_chart.png')
@@ -86,9 +101,11 @@ def generate_monthly_sales_chart(data):
 
 
 def main():
-	result = columnes_count('customers', '2022-10-01', '2023-03-01', 'purchase')
-	generate_time_series_chart(result)
-	generate_monthly_sales_chart(result)
+    # create table
+    # create_query_date('customers', 'total_sales', '2022-10-01', '2023-03-01', 'purchase')
+    result = query_total_sales('total_sales')
+    generate_time_series_chart(result)
+    generate_monthly_sales_chart(result)
  
 
 if __name__ == "__main__":
