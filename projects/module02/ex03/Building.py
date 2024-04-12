0dd3f04656e55caa4e05a5b874e7196a5bae07fe
Juan angel Gonzales laura <juan-gon@c3r13s6.42madrid.com>
2024-04-12 21:01:47 +0200

    ex03

--- projects/module02/ex03/Building.py
+++ projects/module02/ex03/Building.py
@@ -18,38 +18,51 @@ except SQLAlchemyError as e:
     print(f"Error: {e}")
     
     
-def frequency(table: str)-> None:
+def frequency(table: str) -> None:
 	try:
 		start_time = time.time()
-		sql_command = f"""
-            SELECT
+		sql_command = """
+		SELECT
 			user_id,
-			SUM(price) AS total_spent
-			FROM
+			COUNT(*) AS frequency
+		FROM
 			customers
-			WHERE
+		WHERE
 			event_type = 'purchase'
-			GROUP BY
-			user_id
-			ORDER BY
-			total_spent;
-					"""
+		GROUP BY
+			user_id;
+		""".format(table)
+		
 		with engine.connect() as conn:
 			df = pd.read_sql(sql_command, conn)
+		
+		# Agrupamos por frecuencia		
+		df['frequency_group'] = pd.cut(df['frequency'], bins=range(0, df['frequency'].max() + 10, 10), right=False)
+		grouped_df = df.groupby('frequency_group', observed=True).size().reset_index(name='number_of_users')
+
+		mean_frequency = grouped_df['number_of_users'].mean()
+		
+		# Filtramos para obtener solo las frecuencias mayores o iguales a la media
+		filtered_df = grouped_df[grouped_df['number_of_users'] >= mean_frequency]
 		end_time = time.time()
 		print(f"Elapsed time: {end_time - start_time:.2f} seconds\n")
+		print(filtered_df.head(10))
+		plt.figure(figsize=(10,10))
 
-		plt.figure(figsize=(10,6))
-		df['total_spent'].hist(bins=10, color='blue', alpha=0.7)
+		plt.bar([str(x.left) + '-' + str(x.right) for x in filtered_df['frequency_group']], filtered_df['number_of_users'])
 		plt.xlabel('Frecuency')
 		plt.ylabel('Customers')
+
+		plt.xticks(rotation=0)
+		
+		plt.tight_layout()
 		plt.savefig("test.png")
 		plt.close()
 			
 	except Exception as e:
 		print(f"Error: {e}")
 		return None
-    
+
 def main():
     frequency('orders')
 
