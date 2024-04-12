eee4ce1a11e6bbab3193137bb2d6322d6a32c49f
Juan angel Gonzales laura <juan-gon@c3r13s6.42madrid.com>
2024-04-12 22:37:36 +0200

    ex03

--- projects/module02/ex03/Building.py
+++ projects/module02/ex03/Building.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
 import seaborn as sns
+import numpy as np
 
 
 
@@ -46,9 +47,9 @@ def frequency(table: str) -> None:
 		filtered_df = grouped_df[grouped_df['number_of_users'] >= mean_frequency]
 		end_time = time.time()
 		print(f"Elapsed time: {end_time - start_time:.2f} seconds\n")
-		print(filtered_df.head(10))
-		plt.figure(figsize=(10,10))
 
+
+		plt.figure(figsize=(10,10))
 		plt.bar([str(x.left) + '-' + str(x.right) for x in filtered_df['frequency_group']], filtered_df['number_of_users'])
 		plt.xlabel('Frecuency')
 		plt.ylabel('Customers')
@@ -56,7 +57,45 @@ def frequency(table: str) -> None:
 		plt.xticks(rotation=0)
 		
 		plt.tight_layout()
-		plt.savefig("test.png")
+		plt.savefig("frequency.png")
+		plt.close()
+			
+	except Exception as e:
+		print(f"Error: {e}")
+		return None
+
+def monetary_value(table: str) -> None:
+	try:
+		start_time = time.time()
+		sql_command = f"""
+		SELECT
+			user_id,
+			SUM(CASE WHEN price > 0 THEN price ELSE 0 END)::NUMERIC AS altairian_dollars_spent
+		FROM
+			{table}
+		WHERE
+			event_type = 'purchase'
+		GROUP BY
+			user_id;
+		"""
+		
+		with engine.connect() as conn:
+			df = pd.read_sql(sql_command, conn)
+		
+		bins = [-24, 24, 74, 124, 174, 224]
+		labels = ["0", "50", "100", "150", "200"]
+		df['range'] = pd.cut(df['altairian_dollars_spent'], bins=bins, labels=labels, include_lowest=True)
+
+		customer_count = df['range'].value_counts().sort_index().reset_index(name='customers')
+		customer_count.columns = ['range', 'customers']
+
+		plt.figure(figsize=(10, 6))
+		plt.bar(customer_count['range'], customer_count['customers'], color='skyblue')
+		plt.xticks(rotation=0)
+		plt.ylim(0, customer_count['customers'].max() * 1.1)
+		plt.xlabel('Monetary Value in $')
+		plt.ylabel('Customers')
+		plt.savefig("monetary_value.png")
 		plt.close()
 			
 	except Exception as e:
@@ -64,7 +103,8 @@ def frequency(table: str) -> None:
 		return None
 
 def main():
-    frequency('orders')
+    frequency('customers')
+    monetary_value('customers')
 
 if __name__ == '__main__':
     main()
